cc50fcaddb512af32b1bc1c7d2991c00006b3080
Erick Gabriel <erickgabrielalves@hotmail.com>
2020-05-30 19:04:59 -0300

    Função para as posições

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,9 @@ import { ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { analyzeAndValidateNgModules, ThrowStmt } from '@angular/compiler';
 
+/* Importação da Interface ILocal */
+import { ILocal } from '../interfaces/ILocal';
+
 /* Variavel que faz a biblioteca do Google operar */
 /* Biblioteca script do index.html */
 declare var google: any;
@@ -20,6 +23,35 @@ export class HomePage {
   /* Variável do mapa */
   map: any;
   posicaoAtual: any;
+
+  /* Variável que armazena as posições do mapa */
+  public listaLocais:ILocal[] = [
+    {
+      lat: -22.4941369,
+      lng: -48.5525349,
+      titulo: "Q-tall Pizzaria e Restaurante"
+    },
+    {
+      lat: -22.4982226,
+      lng: -48.5642647,
+      titulo: "Batata Mania"
+    },
+    {
+      lat: -22.4990042,
+      lng: -48.5571168,
+      titulo: "Churrasco Grego"
+    },
+    {
+      lat: -22.4941396,
+      lng: -48.5599565,
+      titulo: "Colégio Tyto Objetivo"
+    },
+    {
+      lat: -22.498255,
+      lng: -48.561198,
+      titulo: "Chiquinho Sorvetes"
+    }
+  ];
 
   /* @ViewChild Pega o elemento mapa da home.page.html e coloca elementos filhos dentro dela */
   /* 'mapRef' faz referência ao '#map' da home.page.html */
@@ -52,49 +84,26 @@ export class HomePage {
       title: "Minha posição atual", /* Aparece quando coloco o mouse sobre o marcador */
       animation: google.maps.Animation.BOUNCE
     });
+  
+    /* Adiciona os marcadores ao mapa */
+    for(let local of this.listaLocais){
+      this.adicionarMarcador(local);
+    }
+  
+  }
+  
+  /* Método que adiciona os marcadores no mapa */
+  private adicionarMarcador(local: ILocal) {
+    /* Desestruturando o objeto */
+    const { lat, lng, titulo } = local;
 
-    /* Variável que guarda a localização */
-    /* Latitude e Longitude */
-    const location2 = new google.maps.LatLng(-22.4941369,-48.5525349);
-    const marcador2 = new google.maps.Marker({
-      position: location2, /* Marca a posição */
+    const marcador = new google.maps.Marker({
+      position: { lat, lng}, /* Marca a posição */
       map: this.map, /* Fala em qual mapa ele vai colocar o marcador */
-      title: "Q-tall Pizzaria e Restaurante", /* Aparece quando coloco o mouse sobre o marcador */
-      animation: google.maps.Animation.BOUNCE
-    });
-
-    const location3 = new google.maps.LatLng(-22.4982226,-48.5642647);
-    const marcador3 = new google.maps.Marker({
-      position: location3, /* Marca a posição */
-      map: this.map, /* Fala em qual mapa ele vai colocar o marcador */
-      title: "Batata Mania", /* Aparece quando coloco o mouse sobre o marcador */
-      animation: google.maps.Animation.BOUNCE
-    });
-
-    const location4 = new google.maps.LatLng(-22.4990042,-48.5571168);
-    const marcador4 = new google.maps.Marker({
-      position: location4, /* Marca a posição */
-      map: this.map, /* Fala em qual mapa ele vai colocar o marcador */
-      title: "Churrasco Grego", /* Aparece quando coloco o mouse sobre o marcador */
-      animation: google.maps.Animation.BOUNCE
-    });
-
-    const location5 = new google.maps.LatLng(-22.4941396,-48.5599565);
-    const marcador5 = new google.maps.Marker({
-      position: location5, /* Marca a posição */
-      map: this.map, /* Fala em qual mapa ele vai colocar o marcador */
-      title: "Colégio Tyto Objetivo", /* Aparece quando coloco o mouse sobre o marcador */
-      animation: google.maps.Animation.BOUNCE
-    });
-
-    const location6 = new google.maps.LatLng(-22.498255,-48.561198);
-    const marcador6 = new google.maps.Marker({
-      position: location6, /* Marca a posição */
-      map: this.map, /* Fala em qual mapa ele vai colocar o marcador */
-      title: "Chiquinho Sorvetes", /* Aparece quando coloco o mouse sobre o marcador */
-      animation: google.maps.Animation.BOUNCE
+      title: { titulo }, /* Aparece quando coloco o mouse sobre o marcador */
     });
   }
+
 
   /* Chamando o método */
   /* A função shoowMap() só funciona depois que o HTML está pronto */
---
+++ src/app/interfaces/ILocal.ts
@@ -0,0 +1,6 @@
+/* A Interface é uma regra que precisa ser seguida para criar um determinado elemento */
+export interface ILocal{
+    lat: number;
+    lng: number;
+    titulo: string;
+}
